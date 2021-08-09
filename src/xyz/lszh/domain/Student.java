6c896f70998b34382729a1c252fa322f07a45acc
ZhengHang <1749198910@qq.com>
2021-08-09 23:52:52 +0800

    new commit

--- src/xyz/lszh/domain/Student.java
+++ src/xyz/lszh/domain/Student.java
@@ -6,4 +6,28 @@ public class Student {
     private Integer age;
     public static void doSome(){}
     public String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
